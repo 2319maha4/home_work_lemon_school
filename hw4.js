4b8e62af08e184b4109324a01a3906fd9fd9537d
2319maha4 <mariiaahieieva@gmail.com>
2020-01-28 08:40:54 +0200

    hw4

--- hw4.js
+++ hw4.js
@@ -1,4 +1,4 @@
-//1.Клонирование массива. Напишите функцию copyArr(arr), которая копирует массив не изменяя иригинал.
+//Клонирование массива. Напишите функцию copyArr(arr), которая копирует массив не изменяя иригинал.
 function isFruit() {
     let fruits = ["Banana", "Orange", "Lemon", "Apple", "Mango"];
     let myBestFruits = fruits.slice(0, 5);
@@ -6,7 +6,7 @@ function isFruit() {
 }
 isFruit();
 
-//2.Преобразование массива в строку. Напишите код, который преобразовывает и объединяет все элементы массива в одно строковое значение. 
+//Преобразование массива в строку. Напишите код, который преобразовывает и объединяет все элементы массива в одно строковое значение. 
 // Элементы массива будут разделены запятой. Получите результат двумя разными методами.
 function getWeatherActivity() {
     let weatherActivity = ['Ветер', 'Дождь', 'Молния', 'Гром', 'Торнадо', 'Цунами'];
@@ -18,7 +18,7 @@ function getWeatherActivity() {
 }
 getWeatherActivity();
 
-//3.Заполните массив 10-ю иксами с помощью цикла.
+//Заполните массив 10-ю иксами с помощью цикла.
 function fillArrayX() {
     let arrX = [];
     for (let arrNum = 0; arrNum < 10; arrNum++) {
@@ -28,7 +28,7 @@ function fillArrayX() {
 }
 fillArrayX();
 
-//4.Заполните массив числами от 1 до 10 с помощью цикла
+//Заполните массив числами от 1 до 10 с помощью цикла
 function fillArrayY() {
     let arrY = [];
     for (let numStart = 1; numStart <= 10; numStart++) {
@@ -38,7 +38,7 @@ function fillArrayY() {
 }
 fillArrayY();
 
-//5.Заполните массив 10-ю случайными числами (дробями) от 0 до 1 с помощью цикла. Дроби округляйте до двух знаков в дробной части.
+//Заполните массив 10-ю случайными числами (дробями) от 0 до 1 с помощью цикла. Дроби округляйте до двух знаков в дробной части.
 function fillArrayD() {
     let arrD = [];
     for (let numInHere = 0; numInHere < 10; numInHere++) {
@@ -48,7 +48,7 @@ function fillArrayD() {
 }
 fillArrayD();
 
-//6.Заполните массив 10-ю случайными числами от 1 до 10 с помощью цикла. 
+//Заполните массив 10-ю случайными числами от 1 до 10 с помощью цикла. 
 function fillArrayC() {
     let arrC = [];
     for (let someNumber = 0; someNumber < 10; someNumber++) {
@@ -58,7 +58,7 @@ function fillArrayC() {
 }
 fillArrayC();
 
-//7.Дан массив с числами. С помощью цикла выведите только те элементы массива, которые больше нуля и меньше 10-ти. 
+//Дан массив с числами. С помощью цикла выведите только те элементы массива, которые больше нуля и меньше 10-ти. 
 function getFilter() {
     let numbers = [1, 2, 4, -1, -3, 8, 7, 0, -5, 18, 12];
     let num1 = numbers.filter(number => number > 0 && number < 10);
@@ -66,7 +66,7 @@ function getFilter() {
 }
 getFilter();
 
-//8.Дан массив с числами. С помощью цикла проверьте, что в нем есть элемент со значением 5
+//Дан массив с числами. С помощью цикла проверьте, что в нем есть элемент со значением 5
 //Как только будет найден первый такой элемент - выведите 'Есть' и оборвите цикл. Если такого элемента нет - ничего не выводите.
 function getSomeElement() {
     let arrElements = [1, 2, 3, 4, 5, 6, 7, 8, 9];
@@ -80,7 +80,7 @@ function getSomeElement() {
 }
 getSomeElement();
 
-//9.Дан массив с числами. С помощью цикла найдите сумму элементов этого массива.
+//Дан массив с числами. С помощью цикла найдите сумму элементов этого массива.
 function getSumArraysElement() {
     let arrElements = [2, 4, 5, 8];
     arrSumElements = 0;
@@ -91,7 +91,7 @@ function getSumArraysElement() {
 }
 getSumArraysElement();
 
-//10.Дан массив с числами. С помощью цикла найдите сумму квадратов элементов этого массива. 
+//Дан массив с числами. С помощью цикла найдите сумму квадратов элементов этого массива. 
 function getSumSquares() {
     let arrNumber = [2, 4, 5, 8];
     arrSumSquares = 0;
@@ -102,7 +102,7 @@ function getSumSquares() {
 }
 getSumSquares();
 
-//11.Дан массив с числами. Найдите среднее арифметическое его элементов (сумма элементов, делить на количество)
+//Дан массив с числами. Найдите среднее арифметическое его элементов (сумма элементов, делить на количество)
 function getArithmeticMean() {
     let arrNumbers = [2, 4, 5, 8];
     arrArithmeticMeanResult = 0;
@@ -114,20 +114,220 @@ function getArithmeticMean() {
 }
 getArithmeticMean();
 
-//12.Пользователь вводит многозначное число через promt. Напишите функцию colonOdd(num), 
+//Пользователь вводит многозначное число через promt. Напишите функцию colonOdd(num), 
 // которая принимает число num в качестве аргумента и вставляет двоеточие (:) между двумя нечетными числами. 
-// const num = prompt();
-// function colonOdd(num) {
-//   let result = [num[0]];
-//   for(let i=1; i<num.length; i++) {
-//       if((num[i-1]%2 !== 0)&&(num[i]%2 !== 0)) {
-//         result.push(':', num[i]);
-//        }
-//       else {
-//         result.push(num[i]);
-//       }
-//   }
-//   return result.join('');  
-// }
-// colonOdd(num);
+// НЕ РАБОТАЕТ
+function colonOdd() {
+    const num = prompt();
+    let result = [num[0]];
+    for (let i = 1; i < num.length; i++) {
+        if ((num[i - 1] % 2 !== 0) && (num[i] % 2 !== 0)) {
+            result.push(':', num[i]);
+        } else {
+            result.push(num[i]);
+        }
+    }
+    return result.join('');
+}
+colonOdd();
 
+//Напишите функцию getFirst(array, n), которая возвращает фрагмент массива, содержащий первые 'n' элементов массива.
+function getFirst(array, n) {
+    let firstArr = [23, 245, 6876, 32, 68, 3, 5, 90];
+    let secondArr = firstArr.slice(-4);
+    return secondArr;
+}
+getFirst();
+
+//Сумма элементов двух массивов 
+// НЕ РАБОТАЕТ
+function getSumOfTwoArrays() {
+    let arr1 = [2, 4, 6, 87, 9];
+    let arr2 = [3, 6, 9, 12];
+    let arr3 = arr1.concat(arr2);
+    let sumArraysElement = 0;
+    for (let numInThisFunction = 0, numInThisFunction < arr3.length; numInThisFunction++) {
+        sumArraysElement += arr3[numInThisFunction];
+    }
+    return sumArraysElement;
+}
+getSumOfTwoArrays();
+
+//Напишите код, который создаёт массив элементов представляющих собой сумму соответствующих элементов заданных массивов.
+// НЕ РАБОТАЕТ
+function getNewArrFromArraysElementsSum() {
+    let arrNum1 = [1, 2, 3, 4, 5];
+    let arrNum2 = [4, 5, 6];
+    let arrNum3 = [];
+    if (arrNum1.length > arrNum2.length) {
+        let count = arrNum1.length;
+    } else {
+        let count = arrNum2.length;
+    }
+    for (let i = 0; i < count; i++) {
+        if (arrNum2[i] === undefined) {
+            arrNum2.push(0);
+        } else {
+            if (arrNum1[i] === undefined) {
+                arrNum1.push(0);
+            }
+        }
+        arrNum3.push(arrNum1[i] + arrNum2[i]);
+    }
+    return arrNum3
+}
+getNewArrFromArraysElementsSum();
+
+//Напишите функцию compareNumbers(arr), которая возвращает массив, элементы которого отсортированы по убыванию их значений.
+function compareNumbersArray(arrInHere) {
+    arrInHere = arrInHere.sort(function (a, b) {
+        return a - b
+    })
+    console.log(arrInHere);
+}
+let arrToCompare = [2, 45, 56, 78, 0];
+compareNumbersArray(arrToCompare);
+
+//Напишите код, который определяет сумму и произведение значений массива.
+function getSumAndProductOfArray() {
+    let arrayNum1 = [2, 4, 5, 8];
+    let arrayNum2 = [4, 9, 2, 1];
+    let arrayNum3 = arrayNum1.concat(arrayNum2);
+    let resultForSum = 0;
+    let resultForProduct = 1;
+    for (let startNumber = 0; startNumber < arrayNum3.length; startNumber++) {
+        resultForSum += arrayNum3[startNumber];
+    }
+    for (let startNumber = 0; startNumber < arrayNum3.length; startNumber++) {
+        resultForProduct *= arrayNum3[startNumber];
+    }
+    return "arrays sum =" + resultForSum + "," + "arrays product =" + resultForProduct;
+}
+getSumAndProductOfArray();
+
+//Напишите функцию filterFalse(arr), 
+// которая очищает массив от ложных (false) значений: false, null, undefined, 0, –0, NaN и "" (пустя строка).
+// НЕ РАБОТАЕТ NaN
+function filterFalse() {
+    arrForFilter = [NaN, 0, 77, false, -17, '', undefined, 99, null];
+    let FilteredArray = arrForFilter.filter(elem => elem !== false &&
+        elem !== null &&
+        elem !== undefined &&
+        elem !== NaN &&
+        elem !== '' &&
+        elem !== 0 &&
+        elem !== -0
+    )
+
+    return FilteredArray;
+}
+filterFalse();
+
+//Задача. Дан массив ['a', 'b', 'c']. Добавьте ему в конец элементы 1, 2, 3.
+function pushElements() {
+    let arrToPush = ['a', 'b', 'c'];
+    let newArrToPush = arrToPush.push(1, 2, 3);
+    return arrToPush;
+}
+pushElements();
+
+//Дан массив [1, 2, 3]. Добавьте ему в начало элементы 4, 5, 6.
+function unshiftElements() {
+    let arrToUnshift = [1, 2, 3];
+    let newArrToUnshift = arrToUnshift.unshift(4, 5, 6);
+    return arrToUnshift;
+}
+unshiftElements();
+
+//Дан массив ['js', 'css', 'jq']. Выведите на экран последний элемент.
+function popElement() {
+    let arrayToPop = ['js', 'css', 'jq'];
+    console.log(arrayToPop.pop())
+}
+popElement();
+
+//Дан массив [1, 2, 3]. Сделайте из него массив [3, 2, 1].
+function reverseArray() {
+    let arrayToReverse = [1, 2, 3];
+    arrayToReverse.reverse();
+    return arrayToReverse;
+}
+reverseArray();
+
+//Дан массив [1, 2, 3, 4, 5]. С помощью метода slice запишите в новый элементы [4, 5].
+function sliceSomeNum() {
+    let arrToSliceSomeNum = [1, 2, 3, 4, 5];
+    let newArrToSliceSomeNum = arrToSliceSomeNum.slice(3, 5);
+    return newArrToSliceSomeNum;
+}
+sliceSomeNum();
+
+//Дан массив [1, 2, 3, 4, 5]. С помощью метода splice преобразуйте массив в [1, 4, 5].
+function spliceSomeElements() {
+    let arrToSplice = [1, 2, 3, 4, 5];
+    let newArrToSplice = arrToSplice.splice(1, 2);
+    return arrToSplice;
+}
+spliceSomeElements();
+
+//Дан массив [1, 2, 3, 4, 5]. С помощью метода splice запишите в новый массив элементы [2, 3, 4].
+function spliceSomeElements2() {
+    let arrToSplice2 = [1, 2, 3, 4, 5];
+    let newArrToSplice2 = arrToSplice2.splice(0, 1);
+    let newArrToSplice3 = arrToSplice2.splice(3, 1);
+    return arrToSplice2;
+}
+spliceSomeElements2();
+
+//Создайте массив arr = ['a', 'b', 'c']. Выведите его на экран с помощью функции alert.
+function showArray() {
+    let arrToSchow = ['a', 'b', 'c'];
+    alert(arrToSchow);
+}
+showArray();
+
+// С помощью массива arr из предыдущего номера выведите на экран содержимое первого, второго и третьего элементов.
+function showArray1() {
+    let arrToSchow1 = ['a', 'b', 'c'];
+    console.log(arrToSchow1[0] + arrToSchow1[1] + arrToSchow1[2]);
+}
+showArray1();
+
+
+//Создайте массив arr = ['a', 'b', 'c', 'd'] и с его помощью выведите на экран строку 'a+b, c+d'.
+function showArray1() {
+    let arrayToShow = ['a', 'b', 'c', 'd'];
+    alert(arrayToShow[0] + "+" + arrayToShow[1] + ', ' + arrayToShow[2] + "+" + arrayToShow[3]);
+}
+showArray1();
+
+// Дан объект {js:['jQuery', 'Angular'], php: 'hello', css: 'world'}. Выведите с его помощью слово 'jQuery'.
+let obj = {
+    js: ['jQuery', 'Angular'],
+    php: 'hello',
+    css: 'world'
+};
+obj.js[0];
+
+// Дан массив [ [1, 2, 3], [4, 5, 6], [7,8,9] ]. Выведите на экран цифру 4 из этого массива.
+function schowNumberFromArray() {
+    let arrayInThisTask = [
+        [1, 2, 3],
+        [4, 5, 6],
+        [7, 8, 9]
+    ];
+    console.log(arrayInThisTask[1][0]);
+}
+schowNumberFromArray();
+
+// Напишите функцию generateNumbers(start, len), 
+// которая генерирует массив заданной длины len, заполненный целыми числами, где каждое число больше предыдущего на единицу.
+// НЕ ПОНЯТНО
+function generateNumbers(start, len) {
+    let arrToGenerate = new Array(5);
+    for (let i = 0; i < len; i++, start++) {
+        arrToGenerate[i] = start;
+    }
+    return arrToGenerate;
+}
+generateNumbers(1, 5);
