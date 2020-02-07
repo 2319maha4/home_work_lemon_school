9c72b29c7923b39572c1e59f701b2d2cade3e894
Maria Ageeva <mariiaahieieva@gmail.com>
2020-02-07 16:03:03 +0200

    hw8

--- hw8.js
+++ hw8.js
@@ -3,12 +3,11 @@
 year (год поступления в вуз). Класс должен иметь метод getFullName() (наследуется от User),выводит  имя и фамилию студента. +
 Также класс должен иметь метод getCourse(), выводит текущий курс студента (от 1 до 5). - А ЕСЛИ БОЛЬШЕ 5, МОЖНО ИСПОЛЬЗОВАТЬ IF?
 Курс вычисляется так: нужно от текущего года отнять год поступления в вуз. Текущий год получите самостоятельно.
-Класс Clock написан в функциональном стиле. Перепишите его, используя современный синтаксис классов. - НЕ ПОНИМАЮ
+Класс Clock написан в функциональном стиле. Перепишите его, используя современный синтаксис классов. 
 P.S. Часики тикают в консоли. Откройте её, чтобы посмотреть.*/
 
 // ВОПРОСЫ
 // можно в синтаксисе использовать if(если значение больше 5), не может же, например, человек учиться в вузе 10 лет
-// Класс Clock написан в функциональном стиле. Перепишите его, используя современный синтаксис классов. - НЕ ПОНИМАЮ ЧТО ДЕЛАТЬ
 // где-то я накосячила и все это не работает, пишет "Cannot read property 'getFullYear' of undefined"
 class User {
     constructor(name, surname) {
@@ -36,3 +35,33 @@ class Student extends User {
     }
 }
 let student = new Student(2010);
+
+// Класс Clock написан в функциональном стиле. Перепишите его, используя современный синтаксис классов. - НЕ ПОНИМАЮ ЧТО ДЕЛАТЬ
+class Clock {
+    constructor({template}){
+        this.template = template;
+    }
+    render() {
+        let date = new Date();
+        let hours = date.getHours();
+        if (hours < 10) hours = '0' + hours;
+        let mins = date.getMinutes();
+        if (mins < 10) mins = '0' + mins;
+        let secs = date.getSeconds();
+        if (secs < 10) secs = '0' + secs;
+        let output = this.template
+            .replace('h', hours)
+            .replace('m', mins)
+            .replace('s', secs);
+        console.log(output);
+    }
+    stop() {
+        clearInterval(this.timer);
+    }
+    start() {
+        this.render();
+        this.timer = setInterval(() => this.render(), 1000);
+    }
+}
+let clock = new Clock({template: 'h:m:s'});
+clock.start();
