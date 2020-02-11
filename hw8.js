c49c26d03e83031ed58ad1b1dab89b128307b116
Maria Ageeva <mariiaahieieva@gmail.com>
2020-02-11 11:44:15 +0200

    hw8 все работает

--- hw8.js
+++ hw8.js
@@ -1,14 +1,10 @@
 /*Реализуйте класс Student (Студент), который будет наследовать от класса User,
 класс должен иметь свойства: name (наследуется от User), surname (наследуется от User),
-year (год поступления в вуз). Класс должен иметь метод getFullName() (наследуется от User),выводит  имя и фамилию студента. +
-Также класс должен иметь метод getCourse(), выводит текущий курс студента (от 1 до 5). - А ЕСЛИ БОЛЬШЕ 5, МОЖНО ИСПОЛЬЗОВАТЬ IF?
+year (год поступления в вуз). Класс должен иметь метод getFullName() (наследуется от User),выводит  имя и фамилию студента. 
+Также класс должен иметь метод getCourse(), выводит текущий курс студента (от 1 до 5). 
 Курс вычисляется так: нужно от текущего года отнять год поступления в вуз. Текущий год получите самостоятельно.
 Класс Clock написан в функциональном стиле. Перепишите его, используя современный синтаксис классов. 
 P.S. Часики тикают в консоли. Откройте её, чтобы посмотреть.*/
-
-// ВОПРОСЫ
-// можно в синтаксисе использовать if(если значение больше 5), не может же, например, человек учиться в вузе 10 лет
-// где-то я накосячила и все это не работает, пишет "Cannot read property 'getFullYear' of undefined"
 class User {
     constructor(name, surname) {
         this.getFullName(name, surname)
@@ -16,29 +12,28 @@ class User {
     getFullName(name, surname) {
         this.name = name
         this.surname = surname
-        console.log(name + " " + surname)
+         return name + " " + surname
     }
 }
-let user = new User("Maria", "Ageeva")
-
 class Student extends User {
-    constructor(name, surname, yearOfAdmission, currentYear, currentYearOfStudying) {
+    constructor(name, surname, year) {
         super(name, surname)
-        this.getCourse(yearOfAdmission, currentYear, currentYearOfStudying)
+        this.year = year
+        this.getCourse()
     }
-    getCourse(yearOfAdmission, currentYear, currentYearOfStudying) {
-        this.yearOfAdmission = yearOfAdmission
-        this.currentYear = new Date()
-        currentYear = currentYear.getFullYear()
-        this.currentYearOfStudying = yearOfAdmission - currentYear
-        console.log(currentYearOfStudying)
+    getCourse() {
+        let now = new Date()
+        let nowYear = now.getFullYear()
+        console.log(nowYear - this.year)
     }
 }
-let student = new Student(2010);
+let student = new Student("Maria", "Ageeva", 2010)
 
-// Класс Clock написан в функциональном стиле. Перепишите его, используя современный синтаксис классов. - НЕ ПОНИМАЮ ЧТО ДЕЛАТЬ
+// Класс Clock написан в функциональном стиле. Перепишите его, используя современный синтаксис классов.
 class Clock {
-    constructor({template}){
+    constructor({
+        template
+    }) {
         this.template = template;
     }
     render() {
@@ -63,5 +58,7 @@ class Clock {
         this.timer = setInterval(() => this.render(), 1000);
     }
 }
-let clock = new Clock({template: 'h:m:s'});
+let clock = new Clock({
+    template: 'h:m:s'
+});
 clock.start();
